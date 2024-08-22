614729d7c4d11449cfe763a6c575af2020e0ad95
PHM1605 <PHM1605@gmail.com>
2024-08-23 00:29:50 +0700

    updates

--- build-server/server.js
+++ build-server/server.js
@@ -3,25 +3,13 @@ const express = require('express');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 // custome middleware logger
 app.use(logger)
-
-// cross-origin resource sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:3500'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions))
 
 // middleware for form data
@@ -33,9 +21,12 @@ app.use(express.json())
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
-app.use('/', require('./routes/root'))
+// routes
+app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
+app.use('/register', require('./routes/register'));
+app.use("/auth", require("./routes/auth"));
 
 app.all("*", (req, res) => {
   res.status(404);
